1cc69831a7f96060d82f1334d2b2566b92b14754
BenjaminJPerry <benjamin.j.perry@gmail.com>
2022-02-01 16:49:36 +1300

    Phyre2Prep Version 1.1 - Functional

---
+++ src/ContigsFilter.R
@@ -0,0 +1,67 @@
+#!/usr/bin/env Rscript
+# 2022 Benjamin J Perry & Steph Waller - Attribution-NonCommercial-ShareAlike 4.0 International
+# (CC BY-NC-SA 4.0)
+# Version: 1.0.0
+# Maintainer: Benjamin J Perry
+# Email: benjamin.perry@otago.ac.nz
+# Status: Development
+
+# Description: This script takes and assembly from trinity and the virust summary out put table,
+# it filters for contigs with no BLAST results and a minimum lenght and writes the corresponding
+# contigs into a new file for translation prior to Phyre2 analysis.
+
+# Usage: phyrePrep -in summary.txt -fast trinity.fasta -min 1000 -max 1200
+
+# Import libraries that we need
+if (!require("BiocManager", quietly = TRUE))
+        install.packages("BiocManager")
+
+BiocManager::install("Biostrings")
+
+library(tidyverse) # dataframe manipution and IO
+library(Biostrings) # https://bioconductor.org/packages/release/bioc/html/Biostrings.html
+
+# Parse arg
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)==0) {
+        stop("Error: No command line arguments given. Usage: phyrePrep metatreanscriptome.summary.txt trinity.fasta", call.=FALSE)
+        }
+
+minSize = 1000
+virusTableIn <- args[1]
+fastFileIn <- args[2]
+# TODO : implement minSize variable in the output file name.
+
+outputFile <- "filtered.trinity.len.1000.contigs.fasta"
+# Read the virus summary table
+
+virusTable <- read_tsv(file = virusTableIn, 
+                       col_names = FALSE, 
+                       trim_ws = TRUE, 
+                       progress = TRUE,
+                       col_types = "icidcccddccccddccdd")
+
+# Read the transcriptome contigs (Trinity Assembly)
+trinityContigs <- readDNAStringSet(filepath = fastFileIn,
+                                   format = "fasta",
+                                   use.names = TRUE)
+
+# Filter the virus summary table for entries:
+# 1. which have NO BLAST results
+# 2. have length >= minSize
+filteredVirusTable <- virusTable %>% filter(is.na(X7) & is.na(X13) & is.na(X17)) %>% filter(X3 >= minSize)
+
+# extract contigs names from filteres virus summary table
+filteredContigsNames <- filteredVirusTable$X2
+
+# Take our filtered contigs names from virus summary table and
+# select the corresponding fasta sequences
+
+filteredContigs <- trinityContigs[filteredContigsNames]
+
+# Print out the filtered contigs
+
+writeXStringSet(x = filteredContigs,
+                filepath = outputFile,
+                append = FALSE,
+                format = "fasta")
--- src/getORF.sh
+++ src/getORF.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 
-# 2020 Benjamin J Perry and Steph Waller - Attribution-NonCommercial-ShareAlike 4.0 International
+# 2022 Benjamin J Perry and Steph Waller - Attribution-NonCommercial-ShareAlike 4.0 International
 # (CC BY-NC-SA 4.0)
-# Version: 1
+# Version: 1.1
 # Maintainer: Benjamin J Perry
-# Email: benjamin.perry@postgrad.otago.ac.nz
-# Status: Development
+# Email: benjamin.perry@otago.ac.nz
+# Status: Functional
 
 printf "Starting getORF translation of filtered contigs...\n\n"
 printf "Begin Execution at: $(date)\n\n"
 sleep 1
 
-###  Check for read files ###
+
+###  Check files ###
 if [ "$1" == "" ]; then
-	printf "Error: No trimmed contig file passed\n"
-  printf "Usage: getORF inputfile.fasta outputfile.faa\n\n"
+	printf "Error: No command line arguments given.\n\n"
+  printf "Usage: getORF inputfile.fasta outputfile.faa LENGTH\n\n"
 	exit 1
 fi
 
-TODO: Fix the error message
-if [ $(wc -l $1) -eq 0 ]; then
-	printf "Error: No contigs in the file :( )\n"
-	exit 1
-fi
-
-if [ "$2" == 0 ]; then
-	printf "Error: No protein outfile named :( )\n"
-  printf "Usage: getORF inputfile.fasta outputfile.faa\n\n"
-	exit 1
-fi
+# if [ "$1" == "" ]; then
+# 	printf "Error: No trimmed contig file.\n\n"
+#   printf "Usage: getORF inputfile.fasta outputfile.faa LENGTH\n\n"
+# 	exit 1
+# fi
+# if [ "$2" == "" ]; then
+# 	printf "Error: No protein output file given.\n\n"
+#   printf "Usage: getORF inputfile.fasta outputfile.faa LENGTH\n\n"
+# 	exit 1
+# fi
+# if [ "$3" == "" ]; then
+# 	printf "Error: No minimum contig length given.\n\n"
+#   printf "Usage: getORF inputfile.fasta outputfile.faa LENGTH\n\n"
+# 	exit 1
+# fi
 
 CONTIGS=$1
 PROTEINS=$2
+LENGTH=$3
 
-getorf -minsize 1200 -outseq $PROTEINS -sequence $CONTIGS
-
-printf "Completed getorf translations.\n\n"
+getorf -minsize $LENGTH -outseq $PROTEINS -sequence $CONTIGS
